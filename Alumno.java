25f7c78e03f0fd5f8e9539faeb3195e207364cd6
Marco Galvan <marco_sasori@hotmail.com>
2018-08-31 04:48:14 -0500

    Intercambio inicial de proyecto

---
+++ Alumno.java
@@ -0,0 +1,18 @@
+public class Alumno
+{
+    String nombre;
+    int clave;
+    
+    public Alumno(String nombreA,int num)
+    {
+        nombre=nombreA;
+        clave=num;
+    }
+    
+    public String toString()
+    {
+        String aux = nombre + "," + clave;
+        return aux;
+    }
+    
+} 
